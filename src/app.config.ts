efde0965d15d84865f27b67f7e8daf1b298e17ea
戴海洋 <1037306928@qq.com>
2022-01-15 21:26:34 +0800

    添加注释

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,5 @@
-// RN端不支持defineAppConfig函数
-// export default defineAppConfig({})
+// RN端不支持 export default defineAppConfig({})
+// Taro的RN端需要https://github.com/wuba/Taro-Mortgage-Calculator的安卓ios壳文件（将taro编译的js发送到这个RN壳文件中渲染）
 export default {
 	pages: ['pages/index/index'],
 	window: {
